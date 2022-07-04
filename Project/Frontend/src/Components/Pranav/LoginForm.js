b2a0926e37780e6af7d837f0730cc1391892ba06
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-07-05 00:04:02 +0530

    Modify LoginForm.js to remove code repeatition

--- Project/Frontend/src/Components/Pranav/LoginForm.js
+++ Project/Frontend/src/Components/Pranav/LoginForm.js
@@ -1,62 +1,65 @@
-import React, {useState} from 'react'
-import '../../Styles/Pranav/login.css'
-import Button from '../Pranav/Button';
-import Text_Entry from './Text_Entry';
+import React, { useState } from "react";
+import "../../Styles/Pranav/login.css";
+import Button from "../Pranav/Button";
+import Text_Entry from "./Text_Entry";
 
 //import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ name: "", email: "", password: "" });
 
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
-
-    const submitForm = e => {
-        e.preventDefault();
-        Login(details);
-    }
+  const submitForm = (e) => {
+    e.preventDefault();
+    Login(details);
+  };
 
   return (
     //<Router>
-    <form onSubmit = {submitForm}>
-        <div className="form-inner">
-            <h2><span className = "head">AULA</span></h2>
-            {(error !== "") ? (<div className="error">{error}</div>) : ""}
-            <div className="form-elements">
-                <div className="form-group">
-                    <label htmlFor = "name">Name :</label>
-                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name : e.target.value })} value = {details.name}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor = "email">Email :</label>
-                    <input type="email" name="email" id="email" onChange={e => setDetails({...details, email : e.target.value })} value = {details.email}/>
-                </div>
-                <div className="form-group">
-                <label htmlFor = "password">Password :</label>
-                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password : e.target.value })} value = {details.password}/>
-                </div>
-                <br/>
-                <br/>
-                <Text_Entry prompt={"Username"}></Text_Entry>
-                <Text_Entry prompt={"Password"}></Text_Entry>
+    <form onSubmit={submitForm}>
+      <div className="form-inner">
+        <h2>
+          <span className="head">AULA</span>
+        </h2>
+        {error !== "" ? <div className="error">{error}</div> : ""}
+        <div className="form-elements">
+          <Text_Entry
+            prompt={"Name"}
+            type="text"
+            id="name"
+            name="name"
+            details={details}
+            setDetails={setDetails}
+          ></Text_Entry>
+          <Text_Entry
+            prompt={"Email"}
+            type="email"
+            id="email"
+            name="email"
+            details={details}
+            setDetails={setDetails}
+          ></Text_Entry>
+          <Text_Entry
+            prompt={"Password"}
+            type="password"
+            id="password"
+            name="password"
+            details={details}
+            setDetails={setDetails}
+          ></Text_Entry>
 
+          <Button prompt={"Log-in"} />
+          <Button prompt={"Sign Up"} />
 
-                <Button prompt={"Log-in"}/> 
-                <Button prompt={"Sign Up"}/> 
-                
-                
-              
-
-                {/* <Router>
+          {/* <Router>
                   <Link to="./Signup-one.js">
                   <button>HELLO</button>
                   </Link>
-                  </Router> */
-                }
-              
-            </div>
+                  </Router> */}
         </div>
+      </div>
     </form>
     //</Router>
-  )
+  );
 }
 
 export default LoginForm;
