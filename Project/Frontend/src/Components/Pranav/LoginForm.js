738b639bddfaf5a22dee8cf645435e78bfc56396
Pranav Chatur <pranavchatur@gmail.com>
2022-07-13 15:08:05 +0530

    Merge during 13th July meet

--- Project/Frontend/src/Components/Pranav/LoginForm.js
+++ Project/Frontend/src/Components/Pranav/LoginForm.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "../../Styles/Pranav/login.css";
 import Button from "./Button/Button";
 import TextEntry from "./Text Entry/TextEntry";
-//import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Wordmark from "./Wordmark/Wordmark";
+import Avatar from "./Avatar/Avatar";
 
 function LoginForm({ Login, error }) {
   const [details, setDetails] = useState({ name: "", email: "", password: "" });
@@ -12,27 +13,29 @@ function LoginForm({ Login, error }) {
     Login(details);
   };
 
-  return (
-    //<Router>
+  const title = "AULA";
 
+  return (
     <form onSubmit={submitForm}>
       <div className="form-inner">
-        <h2>
-          <span className="head">AULA</span>
-        </h2>
-        {error !== "" ? <div className="error">{error}</div> : ""}
+        <Avatar radius={"7.4rem"}/>
+        <Wordmark variation={"headline"} prompt={title}/>
         <div className="form-elements">
+          <hr/>
           <TextEntry prompt={"Name"} type="text" id="name" name="name" details={details} setDetails={setDetails} />
           <TextEntry prompt={"Email"} type="email" id="email" name="email" details={details} setDetails={setDetails} />
           <TextEntry prompt={"Password"} type="password" id="password" name="password" details={details} setDetails={setDetails} />
           <Button prompt={"Log-in"} variation={"solid_btn"} />
           <Button prompt={"Sign Up"} variation={"outline_btn"} />
+          {error !== "" ? <div className="error">{error}</div> : ""}
         </div>
-      </div>
-
-      
+      {/* <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Wordmark prompt={"Word Wall"} variation={"word_wall"}/> */}
+      </div>  
     </form>
-    //</Router>
   );
 }
 
