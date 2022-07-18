3a8fc68bdafd47cfe90b8d38e67e9b6b314f2adf
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-07-18 10:31:19 +0530

    Center LoginPage using Div instead of 'body' tag

--- Project/Frontend/src/Pages/Login Page/LoginPage.js
+++ Project/Frontend/src/Pages/Login Page/LoginPage.js
@@ -40,21 +40,23 @@ const LoginPage = ({setUser, adminUser}) => {
     }
 
     return (
-        <div className="login_page">
-            <div>
-                <Avatar radius={"7.4rem"}/>
-                <Wordmark variation={"headline"} prompt={title}/>
+        <form onSubmit={handleLoginBtn} className="parent_login">
+            <div className="content_login">
+                <div>
+                    <Avatar radius={"7.4rem"}/>
+                    <Wordmark variation={"headline"} prompt={title}/>
+                </div>
+                <div>
+                    <hr/>
+                    <TextEntry prompt={"Name"} type="text" id="name" name="name" details={details} setDetails={setDetails} />
+                    <TextEntry prompt={"Email"} type="email" id="email" name="email" details={details} setDetails={setDetails} />
+                    <TextEntry prompt={"Password"} type="password" id="password" name="password" details={details} setDetails={setDetails} />
+                    <Button prompt={"Log-in"} variation={"solid_btn"} type={"submit"}/>
+                    <Button prompt={"Sign Up"} variation={"outline_btn"} />
+                    {error !== "" ? <div className="error">{error}</div> : ""}
+                </div>
             </div>
-            <div>
-                <hr/>
-                <TextEntry prompt={"Name"} type="text" id="name" name="name" details={details} setDetails={setDetails} />
-                <TextEntry prompt={"Email"} type="email" id="email" name="email" details={details} setDetails={setDetails} />
-                <TextEntry prompt={"Password"} type="password" id="password" name="password" details={details} setDetails={setDetails} />
-                <Button prompt={"Log-in"} variation={"solid_btn"} action={handleLoginBtn} />
-                <Button prompt={"Sign Up"} variation={"outline_btn"} />
-                {error !== "" ? <div className="error">{error}</div> : ""}
-            </div>
-        </div>
+        </form>
     );
 }
 
