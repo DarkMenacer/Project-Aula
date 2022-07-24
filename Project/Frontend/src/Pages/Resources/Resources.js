910280283ea1152c4374afe85defc6a00f5097da
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-07-25 02:25:06 +0530

    Add Resources Page (#22)
    * Pull Restructured Repo
    
    * Run on local device
    
    * Modify LoginForm.js to remove code repeatition
    
    * Make no significant change to App.js
    
    * Change formatting a bit
    
    * Restructure
    
    * Remove .vscode Folder
    
    * Change Folder Name
    
    * Add App.js in Gitignore
    
    * Commit During 13th July Meet
    
    * Add Progress Bar using AntD
    
    * Add test.js in .gitignore
    
    * Unknown change in package-lock.json
    
    * Center LoginPage using Div instead of 'body' tag
    
    * Finish SignUp Pages
    
    * Add Participant Home Page
    
    * Add Resources Page

---
+++ Project/Frontend/src/Pages/Resources/Resources.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Table } from "antd";
+import { NavLink } from "react-router-dom";
+import Wordmark from "../../Components/Pranav/Wordmark/Wordmark";
+import { PlusSquareFilled } from "@ant-design/icons"
+import 'antd/dist/antd.min.css';
+import './Resources.css';
+
+const ParticipantHome = () => {
+
+    /* Strings */
+    const informative = "Resources' List";
+
+    /* Component Properties */
+    const columns = [
+        {title: "Sr.no",dataIndex: "srNo"},
+        {title: "Resource name", dataIndex: "resourceName", render:(text) => <NavLink to='/Login'>{text}</NavLink>},
+        {title: "Type", dataIndex: "type"},
+        {title: "Add", dataIndex: "add"}
+    ];
+
+    const data = [
+        {key:"1", srNo:1, resourceName:"First Lesson Notes", type:"PDF", add:<PlusSquareFilled />},
+        {key:"2", srNo:2, resourceName:"Class Video", type:"MP4", add:<PlusSquareFilled />}
+    ];
+
+    /* Functions */
+    const delRow = (e) => {
+        console.log(e.target);
+    }
+
+    return (
+        <div className="parent_part">
+            <div className="content_part">
+                <Wordmark prompt={informative} variation={"direction"}/>
+                <Table columns={columns} dataSource={data} pagination={false} bordered={true}/>
+            </div>
+        </div>
+    );
+}
+
+export default ParticipantHome;
