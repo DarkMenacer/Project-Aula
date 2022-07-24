14b5f5759909db723449881e63aa21072ecbdeca
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-07-25 01:48:29 +0530

    Add Participant Home Page

---
+++ Project/Frontend/src/Pages/Participant Home/ParticipantHome.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Table } from "antd";
+import { NavLink } from "react-router-dom";
+import Wordmark from "../../Components/Pranav/Wordmark/Wordmark";
+import { CloseCircleOutlined } from "@ant-design/icons"
+import 'antd/dist/antd.min.css';
+import './ParticipantHome.css';
+
+const ParticipantHome = () => {
+
+    /* Strings */
+    const informative = "Participant Tab";
+
+    /* Component Properties */
+    const columns = [
+        {title: "Sr.no",dataIndex: "srNo"},
+        {title: "Event name", dataIndex: "eventName", render:(text) => <NavLink to='/Login'>{text}</NavLink>},
+        {title: "Organiser Name", dataIndex: "organiserName"},
+        {title: "Leave", dataIndex: "leave"}
+    ];
+
+    const data = [
+        {key:"1", srNo:1, eventName:"Raghav's Piano Class", organiserName:"Pranav", leave:<CloseCircleOutlined />},
+        {key:"2", srNo:2, eventName:"Pranav's Photography Class", organiserName:"Raghav", leave:<CloseCircleOutlined />}
+    ];
+
+    /* Functions */
+    const delRow = (e) => {
+        console.log(e.target);
+    }
+
+    return (
+        <div className="parent_part">
+            <div className="content_part">
+                <Wordmark prompt={informative} variation={"direction"}/>
+                <Table columns={columns} dataSource={data} pagination={false} bordered={true}/>
+            </div>
+        </div>
+    );
+}
+
+export default ParticipantHome;
